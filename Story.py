ee6dba66dca4a037312ae979ed91e81b5f1a55bd
Avery Fischer <averyfisch13@yahoo.com>
2016-02-09 10:21:51 -0600

    new rough

--- Story.py
+++ Story.py
@@ -9,8 +9,8 @@ print "The Asylum"
 print "By Avery Fischer and Ryan Baraloto."
 print "You have woken up in a abandend asylum."
 print "What course of action would you like to take? Explore or Do nothing."
-action = raw_input()
-if str(action) in Explore:
+action = raw_input("What do you want to do?(Explore/Nothing)")
+if action in Explore:
     print 'You explore the asylum, you hear screaming, do you chose to investigate? (Yes or No)'
     action = raw_input()
     if action in yes:
