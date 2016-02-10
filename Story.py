016d0b2e45909a6724d8c2db874a1c1f2c220f35
Avery Fischer <averyfisch13@yahoo.com>
2016-02-09 21:37:11 -0600

    new rough

--- Story.py
+++ Story.py
@@ -1,4 +1,4 @@
-Explore = {"look around","explore" "look", "adventure","search", "EXPLORE", 'investagate'}
+explore = ["look around","explore" "look", "adventure","search", "EXPLORE", 'investagate']
 stand = {"sit","Do nothing", "do nothing", "STAND"}
 yes = {'yup', 'yea', 'Yes', 'yeah', 'si'}
 no = {'naw', 'No','nope', 'nay'}
@@ -10,10 +10,10 @@ print "By Avery Fischer and Ryan Baraloto."
 print "You have woken up in a abandend asylum."
 print "What course of action would you like to take? Explore or Do nothing."
 action = raw_input("What do you want to do?(Explore/Nothing)")
-if action in "Explore":
+if action in "explore":
     print 'You explore the asylum, you hear screaming, do you chose to investigate? (Yes or No)'
-    action = raw_input()
-    if action in "yes":
+action = raw_input()
+if action in "yes":
         print 'You choose to investigate the screaming, you find that the screamimg is behind a locked door.'
 elif action in stand:
     print 'You chose to sit where you have woken, you hear a faint screaming, do you investigate? (Yes or No)'
